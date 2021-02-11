0c95e6b748310f081da3d3970c8dfc8302b99d75
Rizqillah <71523218+rizqillah-pnl@users.noreply.github.com>
2021-02-11 21:55:19 +0700

    Hasil Belajar Materialize Pertemuan 3 dari WPU

---
+++ Pertemuan 3/js/script.js
@@ -0,0 +1,28 @@
+const sideNav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sideNav);
+
+
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 600,
+  interval: 3000
+});
+
+
+
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax);
+
+
+
+const materialbox = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(materialbox);
+
+
+
+const scrollspy = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scrollspy, {
+  scrollOffset: 50
+});
